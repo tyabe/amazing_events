10cffc1528bc58eaac4e2bba12085690b8fc38df
Takeshi Yabe <tyabe@nilidea.com>
2014-06-22 21:51:49 +0900

    イベント用モデルを追加

--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_many :created_events, class_name: 'Event', foreign_key: :owner_id
+
   def self.find_or_create_from_auth_hash(auth_hash)
     provider  = auth_hash[:provider]
     uid       = auth_hash[:uid]
